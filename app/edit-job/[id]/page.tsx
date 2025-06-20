551e4c60d5b0f7d4579d9ec06baa9a7ebe6fbf0a
jimohsegun1 <jimohsegunj@gmail.com>
2025-06-20 16:43:29 +0100

    edited

--- app/edit-job/[id]/page.tsx
+++ app/edit-job/[id]/page.tsx
@@ -1,28 +1,14 @@
-// import fs from 'fs';
-// import path from 'path';
-// import JobForm from '../../../components/JobForm';
-
-// interface Job {
-//   id: string;
-//   title: string;
-//   company: string;
-//   link: string;
-//   status: string;
-// }
-
-// export default async function EditJobPage({ params }: { params: { id: string } }) {
-//   const filePath = path.join(process.cwd(), 'data', 'jobs.json');
-//   const jobs: Job[] = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-//   const job = jobs.find((j) => j.id === params.id);
-
-//   return <JobForm initialData={job} />;
-// }
-
-
 import fs from 'fs';
 import path from 'path';
-import { Job } from '../../../lib/api';
 import JobForm from '../../../components/JobForm';
+
+interface Job {
+  id: string;
+  title: string;
+  company: string;
+  link: string;
+  status: string;
+}
 
 interface PageProps {
   params: {
@@ -36,8 +22,11 @@ export default async function EditJobPage({ params }: PageProps) {
   const job = jobs.find((j) => j.id === params.id);
 
   if (!job) {
-    // You might want to throw a 404 or redirect
-    return <div className="text-red-500 p-4">Job not found.</div>;
+    return (
+      <div className="p-4 text-red-500">
+        Job not found.
+      </div>
+    );
   }
 
   return <JobForm initialData={job} />;
